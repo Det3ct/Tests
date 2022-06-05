19f68f0e738bda4bcc03574677df9077949b7aec
splincode <omaxphp@yandex.com>
2022-06-05 19:08:28 +0400

    added group creation and registration

--- src/pages/Admin/CreateGroup.jsx
+++ src/pages/Admin/CreateGroup.jsx
@@ -7,17 +7,17 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Autocomplete from '@mui/material/Autocomplete';
 import Stack from '@mui/material/Stack';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import { Box } from '@mui/material';
 
 
 const endpoint = 'http://localhost/api'
 
 const CreateGroup = () => {
-    const [Specialities , setSpecialities] = useState([])
+    const [Specialities, setSpecialities] = useState([])
 
-    useEffect( () => {
+    useEffect(() => {
         getAllSpecialities()
     }, [])
 
@@ -27,30 +27,57 @@ const CreateGroup = () => {
         setSpecialities(response.data)
     }
 
+
+    const [name, setName] = useState('')
+    const [speciality_id, setSpeciality_id] = useState('')
+    const Navigate = useNavigate()
+
+
+    const store = async (e) => {
+        try {
+            e.preventDefault()
+            await axios.post(`${endpoint}/group`, { name: name, speciality_id: speciality_id })
+            Navigate('/');
+        } catch (e) {
+            console.log(e.response)
+
+        } finally {
+
+        }
+    }
     return (
         <React.Fragment>
             <CssBaseline />
             <Container fixed >
                 <Typography mt={4} mb={1} variant="h4" color="initial" align="center">Создать группу</Typography>
                 <Divider />
-                <Stack spacing={2} justifyContent="center">
-                    <TextField fullWidth label="Название" size="small" />
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        options={Specialities}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="Специальности" size='small' />}
-                        getOptionLabel={(option) => `${option.name}`}
-                    />
-                </Stack>
-                <Stack justifyContent="space-between" direction="row" mt={3}>
-                    <Button variant="contained" color="error" component={Link} to={'/'}>Отмена</Button>
-                    <Button variant="contained" color="success">Создать</Button>
-                </Stack>
+                <Box
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={store}
+                >
+                    <Stack spacing={2} justifyContent="center">
+                        <TextField fullWidth label="Название" size="small" value={name} onChange={(e) => setName(e.target.value)} />
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={Specialities}
+                            fullWidth
+                            renderInput={(params) => <TextField {...params} label="Специальности" size='small' />}
+                            getOptionLabel={(option) => `${option.code} ${option.name}`}
+                            onChange={(event, value) => setSpeciality_id(value.id)}
+                        />
+                    </Stack>
+                    <Stack justifyContent="space-between" direction="row" mt={3}>
+                        <Button variant="contained" color="error" component={Link} to={'/'}>Отмена</Button>
+                        <Button variant="contained" color="success" type='submit'> Создать</Button>
+                    </Stack>
+                </Box>
             </Container>
         </React.Fragment>
     )
 }
+
 
 export { CreateGroup };
--- src/pages/Admin/CreateUser.jsx
+++ src/pages/Admin/CreateUser.jsx
@@ -7,8 +7,9 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { Box } from '@mui/material';
 
 
 
@@ -16,21 +17,44 @@ const endpoint = 'http://localhost/api'
 
 const CreateUser = () => {
 
-    const [Specialities , setSpecialities] = useState([])
+    const [Groups, setGroups] = useState([])
 
-    useEffect( () => {
-        getAllSpecialities()
+    useEffect(() => {
+        getAllGroups()
     }, [])
 
 
-    const getAllSpecialities = async () => {
-        const response = await axios.get(`${endpoint}/specialities`)
-        setSpecialities(response.data)
+    const getAllGroups = async () => {
+        const response = await axios.get(`${endpoint}/groups`)
+        setGroups(response.data)
     }
 
 
-    const role = [{ label: 'Админ'},
-    { label: 'Студент'}]
+    const role = [{ id: '1', name: 'Админ' },
+    { id: '2', name: 'Студент' }]
+
+
+    const [firstName, setfirstName] = useState('')
+    const [lastName, setlastName] = useState('')
+    const [patronymic, setPatronymic] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [user_group_id, setUser_group_id] = useState('')
+    const [role_id, setRole_id] = useState('')
+    const Navigate = useNavigate()
+
+    const store = async (e) => {
+        try {
+            e.preventDefault()
+            await axios.post(`${endpoint}/register`, {firstName: firstName ,  lastName: lastName, patronymic: patronymic , user_group_id: user_group_id ,role_id: role_id , email: email , password: password})
+            Navigate('/');
+        } catch (e) {
+            console.log(e.response)
+
+        } finally {
+
+        }
+    }
 
     return (
         <React.Fragment>
@@ -38,34 +62,44 @@ const CreateUser = () => {
             <Container fixed >
                 <Typography mt={4} mb={3} variant="h4" color="initial" align="center">Зарегистрировать пользователя</Typography>
                 <Divider />
+                <Box
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={store}
+                >
                 <Stack spacing={2} justifyContent="center">
-                    <TextField fullWidth label="Имя" size="small" />
-                    <TextField fullWidth label="Фамилия" size="small" />
-                    <TextField fullWidth label="Отчество" size="small" />
-                    <TextField fullWidth label="Email" size="small" />
-                    <TextField fullWidth label="Пароль" size="small" />
+                    <TextField fullWidth label="Имя" size="small" value={firstName} onChange={(e) => setfirstName(e.target.value)}/>
+                    <TextField fullWidth label="Фамилия" size="small"  value={lastName} onChange={(e) => setlastName(e.target.value)}/>
+                    <TextField fullWidth label="Отчество" size="small" value={patronymic} onChange={(e) => setPatronymic(e.target.value)} />
+                    <TextField fullWidth label="Email" size="small" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <TextField fullWidth label="Пароль" size="small" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <Autocomplete
                         disablePortal
                         id="combo-box-demo"
-                        options={Specialities}
+                        options={Groups}
                         sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="Специальность" size='small' />}
+                        renderInput={(params) => <TextField {...params} label="Группа" size='small' />}
                         getOptionLabel={(option) => `${option.name}`}
+                        onChange={(event, value) => setUser_group_id(value.id)}
                     />
                     <Autocomplete
                         disablePortal
                         id="combo-box-demo"
                         options={role}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="Роль" size='small' />}
                         defaultValue={role[1]}
+                        sx={{ width: 300 }}
+                        getOptionLabel={(option) => `${option.name}`}
+                        renderInput={(params) => <TextField {...params} label="Роль" size='small' />}
+                        onChange={(event, value) => setRole_id(value.id)}
                     />
 
                 </Stack>
                 <Stack justifyContent="space-between" direction="row" mt={3}>
                     <Button variant="contained" color="error" component={Link} to={'/'}>Отмена</Button>
-                    <Button variant="contained" color="success">Зарегистрировать</Button>
+                    <Button variant="contained" color="success" type='submit'>Зарегистрировать</Button>
                 </Stack>
+                </Box>
             </Container>
         </React.Fragment>
     )
